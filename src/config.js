c2bc0629714c1349c29f9d14f5d70157f40944ea
AnajaFriday <anajafriday113@gmail.com>
2025-02-02 14:51:24 +0100

    changed api url from local to production

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = "https://jsonkeeper.com/b/97OU";
-export const API_URL = "/";
+export const API_URL = "https://travaye-node-db.onrender.com";
 // export const API_URL = "http://localhost:3000";
